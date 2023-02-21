461a2143ebd402fd5b0bfdf215574962f4c6177f
Bikash GC <bikashgc937@gmail.com>
2023-02-21 13:03:42 +0545

    Added functionality to increase,decrease and reset the score

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int numberA = 0;
+  int numberB = 0;
+
+  void increaseNumberTeamA() {
+    setState(() {
+      numberA++;
+    });
+  }
+
+  void decreaseNumberTeamA() {
+    setState(() {
+      numberA--;
+    });
+  }
+
+  void increaseNumberTeamB() {
+    setState(() {
+      numberB++;
+    });
+  }
+
+  void decreaseNumberTeamB() {
+    setState(() {
+      numberB--;
+    });
+  }
+
+  void resetNumber() {
+    setState(() {
+      numberA = 0;
+      numberB = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,87 +51,92 @@ class _HomePageState extends State<HomePage> {
         title: Text("Score App"),
         centerTitle: true,
       ),
-      body: Row(
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                const Text(
-                  'Team A',
-                  style: TextStyle(
-                    fontSize: 20,
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 20,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  const Text(
+                    'Team A',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    numberA.toString(),
+                    style: TextStyle(
+                      fontSize: 80,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  RaisedButton(
                     color: Colors.red,
-                    fontWeight: FontWeight.bold,
+                    child: const Text(
+                      'Increase',
+                    ),
+                    onPressed: increaseNumberTeamA,
                   ),
-                ),
-                const Text(
-                  '0',
-                  style: TextStyle(
-                    fontSize: 40,
+                  SizedBox(height: 10),
+                  RaisedButton(
                     color: Colors.red,
-                    fontWeight: FontWeight.bold,
+                    child: const Text(
+                      'Decrease',
+                    ),
+                    onPressed: decreaseNumberTeamA,
                   ),
-                ),
-                RaisedButton(
-                  color: Colors.red,
-                  child: const Text(
-                    'increment',
-                  ),
-                  onPressed: () {},
-                ),
-                SizedBox(height: 10),
-                RaisedButton(
-                  color: Colors.red,
-                  child: const Text(
-                    'decrement',
-                  ),
-                  onPressed: () {},
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                const Text(
-                  'Team B',
-                  style: TextStyle(
-                    fontSize: 20,
+            Expanded(
+              child: Column(
+                children: [
+                  const Text(
+                    'Team B',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    numberB.toString(),
+                    style: TextStyle(
+                      fontSize: 80,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  RaisedButton(
                     color: Colors.blue,
-                    fontWeight: FontWeight.bold,
+                    child: const Text(
+                      'Increase',
+                    ),
+                    onPressed: increaseNumberTeamB,
                   ),
-                ),
-                const Text(
-                  '0',
-                  style: TextStyle(
-                    fontSize: 40,
+                  const SizedBox(height: 10),
+                  RaisedButton(
                     color: Colors.blue,
-                    fontWeight: FontWeight.bold,
+                    child: const Text(
+                      'Decrease',
+                    ),
+                    onPressed: decreaseNumberTeamB,
                   ),
-                ),
-                RaisedButton(
-                  color: Colors.blue,
-                  child: const Text(
-                    'increment',
-                  ),
-                  onPressed: () {},
-                ),
-                const SizedBox(height: 10),
-                RaisedButton(
-                  color: Colors.blue,
-                  child: const Text(
-                    'decrement',
-                  ),
-                  onPressed: () {},
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.restore_outlined),
-        onPressed: () {},
+        onPressed: resetNumber,
       ),
     );
   }
